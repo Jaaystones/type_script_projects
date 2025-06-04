e5011b55f3d8c5f9f585172f73c33ddd048ccc3b
Jaaystones <Joelpaul345@gmail.com>
2025-06-04 04:50:05 -0700

    :wrench: updated css

---
+++ nextjs-dashboard/app/ui/fonts.ts
@@ -0,0 +1,16 @@
+import { Inter } from 'next/font/google';
+import { Lusitana } from 'next/font/google';
+
+
+
+export const inter = Inter({
+  subsets: ['latin'],
+  variable: '--font-inter',
+  display: 'swap',
+});
+
+export const lusitana = Lusitana({
+  subsets: ['latin'],
+  variable: '--font-lusitana',
+  weight: ['400', '700']
+});
