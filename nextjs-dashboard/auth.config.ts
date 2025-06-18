1dd2a4fbd5d948e8eb4672e4c2819e515d9dee17
Jaaystones <Joelpaul345@gmail.com>
2025-06-18 06:53:06 -0700

    :wrench: Implemented login and logout functionality.

---
+++ nextjs-dashboard/auth.config.ts
@@ -0,0 +1,23 @@
+import type { NextAuthConfig } from 'next-auth';
+ 
+export const authConfig = {
+  pages: {
+    signIn: '/login',
+  },
+  callbacks: {
+    authorized({ auth, request: { nextUrl } }) {
+      // Checkes if the user is logged in
+      const isLoggedIn = !!auth?.user;
+      const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
+      //Error Handling: If the user is on the dashboard and not logged in, redirect to login page
+      if (isOnDashboard) {
+        if (isLoggedIn) return true;
+        return false; // Redirect unauthenticated users to login page
+      } else if (isLoggedIn) {
+        return Response.redirect(new URL('/dashboard', nextUrl));
+      }
+      return true;
+    },
+  },
+  providers: [], // Add providers with an empty array for now
+} satisfies NextAuthConfig;
